9b257b3a688c3b21961c412d6f73c1952ccc6ce8
Md Minhajul Islam Ishrak <islamminahjul60@gmail.com>
2025-08-14 16:41:13 +0100

    testing fetch

--- azure-function/HttpTrigger/index.js
+++ azure-function/HttpTrigger/index.js
@@ -1,17 +1,4 @@
-// // backend/azure-function/HttpTrigger/index.js
-// const serverless = require('serverless-http');
-// const app = require('../../index'); // path to your exported Express app
-
-// // Optionally set basePath if you want function URL to be /api/...;
-// // leaving default so routes match exactly. You can tune below.
-// const handler = serverless(app, { basePath: '' });
-
-// module.exports = async function (context, req) {
-//   // serverless-http expects (req, res) signature; it adapts for Azure
-//   return handler(context, req);
-// };
-
 const createHandler = require("azure-function-express").createHandler;
-const app = require("../../index"); // path to your main Express app file
+const app = require("../app"); // adjust if your Express app is in another file
 
 module.exports = createHandler(app);
