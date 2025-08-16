b5422c75df74fdc5bfb6b042ae9d8307f94c24f0
Md Minhajul Islam Ishrak <islamminahjul60@gmail.com>
2025-08-16 22:54:32 +0100

    Add serverless-http dependency

--- azure-function/HttpTrigger/index.js
+++ azure-function/HttpTrigger/index.js
@@ -1,5 +1,13 @@
 // azure-function/HttpTrigger/index.js
 const createHandler = require('azure-function-express').createHandler;
 const path = require('path');
-const app = require(path.join(__dirname, '..', '..', 'index')); // require root index.js
+
+// Resolve to the repo-root index.js at runtime
+// __dirname -> C:\home\site\wwwroot\HttpTrigger
+// path.join(__dirname, '..', 'index') -> C:\home\site\wwwroot\index.js
+const appPath = path.join(__dirname, '..', 'index'); 
+console.log('Loading Express app from:', appPath);
+
+const app = require(appPath);
+
 module.exports = createHandler(app);
